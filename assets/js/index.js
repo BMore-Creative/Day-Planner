5da9bba6d6ccbbd2e2e20b239889165f1399f502
Brian Moreno <BMore-Creative@users.noreply.github.com>
2021-10-25 07:19:01 -0700

    added elements: Timeblocks and save btns added for 9am - 5pm

---
+++ assets/js/index.js
@@ -0,0 +1,12 @@
+//DOM element variables
+const currDay = document.querySelector("#currentDay");
+
+
+//Displays current date at top of screen
+currDay.textContent = moment().format("dddd, MMMM Do")
+console.log(currDay.textContent);
+
+for (let i = 0; i < array.length; i++) {
+    const element = array[i];
+    
+}
